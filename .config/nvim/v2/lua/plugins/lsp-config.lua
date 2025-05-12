d1432a1da7a194a8f8ad2771c722c549c5781bb8
Johannes Krabbe <github@Krabbe.dev>
2025-05-12 09:43:15 +0200

    sync

--- .config/nvim/v2/lua/plugins/lsp-config.lua
+++ .config/nvim/v2/lua/plugins/lsp-config.lua
@@ -1,6 +1,7 @@
 return {
     {
         "williamboman/mason.nvim",
+        version = "1.*",
         lazy = false,
         config = function()
             require("mason").setup()
@@ -8,11 +9,14 @@ return {
     },
     {
         "williamboman/mason-lspconfig.nvim",
+        version = "1.*",
         lazy = false,
-        opts = {
-            ensure_installed = { "lua_ls", "ts_ls", "html", "prismals", "eslint", "terraformls", "denols", "tailwindcss", "gopls", "pylsp" }
-            ensure_installed = { "lua_ls", "ts_ls", "html", "prismals", "terraformls", "tailwindcss", "gopls" }
-        },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "ts_ls", "html", "prismals", "eslint", "terraformls", "denols", "tailwindcss", "gopls", "pylsp", "biome" },
+                automatic_enable = true,
+            })
+        end,
     },
     {
         "neovim/nvim-lspconfig",
@@ -21,16 +25,16 @@ return {
             local capabilities = require('cmp_nvim_lsp').default_capabilities()
             local lspconfig = require("lspconfig")
 
-            lspconfig.denols.setup {
+            lspconfig.denols.setup({
                 root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
                 capabilities = capabilities
-            }
+            })
 
-            lspconfig.ts_ls.setup {
+            lspconfig.ts_ls.setup({
                 root_dir = lspconfig.util.root_pattern("package.json"),
                 single_file_support = false,
                 capabilities = capabilities
-            }
+            })
 
             lspconfig.biome.setup({
                 root_dir = lspconfig.util.root_pattern("biome.json"),
@@ -47,35 +51,36 @@ return {
                     }
                 }
             })
+
             lspconfig.lua_ls.setup({
                 capabilities = capabilities
             })
-            --[[
+
             lspconfig.eslint.setup({
                 root_dir = lspconfig.util.root_pattern("eslintrc", ".eslintrc", ".eslintrc.js", ".eslintrc.cjs",
                     ".eslintrc.json"),
                 capabilities = capabilities
             })
-            ]]
+
             lspconfig.terraformls.setup({
                 capabilities = capabilities
             })
+
             lspconfig.html.setup({
                 capabilities = capabilities
             })
+
             lspconfig.tailwindcss.setup({
                 capabilities = capabilities
             })
+
             lspconfig.gopls.setup({
                 capabilities = capabilities
             })
-            lspconfig.rust_analyzer.setup({
-                capabilities = capabilities
-            })
+
             lspconfig.pylsp.setup({
                 capabilities = capabilities
             })
         end,
     },
-
 }
